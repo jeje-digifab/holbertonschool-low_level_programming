9abf0d7a6eb33aaebc63372e6d577b5aec070ab2
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-19 19:17:07 +0100

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
-#include <string.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
 
-int main () {
-    put ("Programming is like building a multilingual puzzle")
-
-     return(0);
+	return (0);
 }
