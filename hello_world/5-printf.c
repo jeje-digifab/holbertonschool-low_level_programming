778baf7fac0d832edadddeacac885a91cad9c80d
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-19 21:11:04 +0100

    printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -12,7 +12,7 @@ int main(void)
  * Return: Always 0.
  */
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 
 	return (0);
 }
