b2657a1e5a222a3cd6d8ad66b81d7c0f9f485466
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-19 21:05:00 +0100

    printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/**
+ * 4-puts.c
+ */
+#include <stdio.h>
+int main(void)
+/**
+ * main() - this is the declaration of the program's,
+ *           main function.
+ * @void:   The void type indicates that there are no arguments,
+ *          in this function.
+ *
+ * Return: Always 0.
+ */
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
