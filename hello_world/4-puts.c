e0b7e76dda98af92525940fcd3ca2ed0891989a2
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-19 23:04:57 +0100

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,13 @@
-/**
- * 4-puts.c
- */
 #include <stdio.h>
-int main(void)
 /**
- * main() - this is the declaration of the program's,
+ * main - this is the declaration of the program's,
  *           main function.
  * @void:   The void type indicates that there are no arguments,
  *          in this function.
  *
  * Return: Always 0.
  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
