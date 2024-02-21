5e44a23c7d5c612ba8458aa08c0208c228dc47b5
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-21 17:36:41 +0100

    print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	char ch;
+	int hex;
+
+	for (hex = 0 ; hex <= 9 ; ++hex)
+	{
+	putchar(hex + '0');
+	}
+
+	for (ch = 'a' ; ch <= 'f' ; ch++)
+	{
+	putchar(ch);
+	}
+
+
+	putchar('\n');
+
+	return (0);
+}
