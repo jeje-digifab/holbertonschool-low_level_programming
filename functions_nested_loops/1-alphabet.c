89f1d5c1a9d2708711940fadd8b4782db410f893
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-22 15:12:24 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <1-main.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
