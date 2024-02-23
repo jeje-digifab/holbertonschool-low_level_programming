f902bda22bb1c69441153c124d89ff8cb1d888e2
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-23 13:52:04 +0100

    isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isalpha - entry point.
+ * @c: fonction putchar.
+ * Return: 0 is Upper.
+ * Return: 1 is Lower.
+ */
+
+int _isalpha(int c)
+
+{
+
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+
+	else if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+
+	else
+	{
+	return (0);
+	}
+
+}
