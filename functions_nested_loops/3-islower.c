72e61035fdf868ec097495135edd6111bf681d49
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-23 13:08:53 +0100

    islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -12,17 +12,13 @@ int _islower(int c)
 {
 
 	if (c >= 97 && c <= 122 )
-/*	if (c >= 'a' && c <= 'z' ) */
 	{
-		_putchar(49);
+	return(1);
 	}
 
-	else if (c >= 'A' && c <= 'Z' )
+	else
 	{
-		_putchar(48);
+	return(0);
 	}
 
-	return (0);
 }
-
-
