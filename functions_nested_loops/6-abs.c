9d0ef8fab8c0fc8a8dd9f5be3870c1a9f606ac1f
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-24 17:19:09 +0100

    abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -12,19 +12,19 @@ int _abs(int n)
 
 {
 
-  if (n > 0)
-  {
-    return (n);
-  }
+	if (n > 0)
+	{
+		return (n);
+	}
 
-  else if (n < '0')
-  {
-    return (- n);
-  }
+	else if (n < '0')
+	{
+		return (- n);
+	}
 
-  else
-  {
-    return ('0');
-  }
+	else
+	{
+		return ('0');
+	}
 
 }
