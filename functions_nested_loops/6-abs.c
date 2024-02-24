4fa1d228c56266070d802bc1a321b0f6f4140b6b
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-24 17:14:19 +0100

    abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _abs - entry point.
+ * @n: value of -abs.
+ * Return: print value absolue of number
+ *			print 0 if r is zero.
+ */
+
+int _abs(int n)
+
+{
+
+  if (n > 0)
+  {
+    return (n);
+  }
+
+  else if (n < '0')
+  {
+    return (- n);
+  }
+
+  else
+  {
+    return ('0');
+  }
+
+}
