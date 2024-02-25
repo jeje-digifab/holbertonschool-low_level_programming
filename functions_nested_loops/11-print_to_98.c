1e7b86f4bc4909bcaf088552825070c288818350
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-25 16:26:49 +0100

    print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - entry point.
+ * @n: value of number.
+ * Return: print value until 98
+ */
+
+void print_to_98(int n)
+
+
+{
+
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+		    printf ("%d", n);
+
+			if (n < 98)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			n++;
+		}
+		printf("\n");
+
+	}
+
+	else
+	{
+
+			while (98 <= n)
+		{
+			printf ("%d", n);
+			putchar(',');
+			putchar(' ');
+			n--;
+		}
+	}
+
+
+
+}
