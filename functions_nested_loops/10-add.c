ff7fcc35ef37dd30643cc931fbe5a5b3b2f54a4c
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-25 01:03:50 +0100

    add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,10 +3,9 @@
 
 /**
  * add - entry point.
- * @n1: value of numbber 1
- * @n2: value of numbber 2
- * Return: print value absolue of number
- *			print 0 if r is zero.
+ * @n1: value of numbber 1.
+ * @n2: value of numbber 2.
+ * Return: additionne n1 et n2.
  */
 
 int add(int n1, int n2)
