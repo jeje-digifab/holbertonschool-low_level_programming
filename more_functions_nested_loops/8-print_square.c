f478526772cd41b63b8cd65dfce96add44e4678a
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 19:07:41 +0100

    print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,28 +10,20 @@
 void print_square(int size)
 
 {
+
 	int i, j;
 
 
-	i = 0;
 
-
-
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
+		for(j = 0; j < size; j++)
+		{
 
+			_putchar('#');
 
-
-		for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-				_putchar('\n');
-				i++;
-
+		}
+		_putchar('\n');
 	}
 
-		_putchar('\n');
 }
-
-
