b4bad1375a80975ee2762c47609fce0c9ca94b21
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 23:23:55 +0100

    Betty

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,11 @@ void print_diagonal(int n)
 	{
 		for( i = 0; i < n; i++)
 		{
-			for( j = 0; j < n; j++)
+			for ( j = 0; j < n; j++)
 			{
-				if(j == i)
+				if (j == i)
 					_putchar('\\');
-				else if (j < i )
+				else if (j < i)
 				{
 					_putchar(' ');
 				}
