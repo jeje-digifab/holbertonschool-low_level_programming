608c11b564917455adc8d42e69cd6bb2dd26cb11
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 10:21:13 +0100

    isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _isdigit - Entry point.
+* @c: variable.
+* Return: Returns 1 if c is digit.
+*		Returns 0 otherwise.
+*/
+
+int _isdigit(int c)
+
+{
+
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
+
+
