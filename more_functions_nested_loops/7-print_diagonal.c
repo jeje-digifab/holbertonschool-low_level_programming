a4cedc188ebd2c57015896ec65903cac8072ff17
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 23:30:08 +0100

    Betty

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
 * print_diagonal - Entry point.
 * @n: is n is the number of times the character \ should be printed.
-* 			n is 0 or less, the function should only print a \n.
+*			n is 0 or less, the function should only print a \n.
 * Return: function that draws a diagonal line on the terminal.
 */
 
