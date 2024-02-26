584a61dcd10834f5a04a6b49f836f3528eaa51ab
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 00:52:48 +0100

    1 of 100

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* more_numbers - Entry point.
+* Return: Prints 10 times the numbers, from 0 to 14.
+*/
+
+int main(void)
+
+{
+	int i;
+
+
+
+		for ( i = 1; i <= 100; i++)
+		{
+			if (i <= 9)
+			{
+				putchar(i + '0');
+				putchar(' ');
+			}
+
+			else if (i > 9 && i < 100)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+			}
+
+			else
+			{
+				putchar((i / 100) + '0');
+				putchar((i % 10) + '0');
+				putchar((i % 10) + '0');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
