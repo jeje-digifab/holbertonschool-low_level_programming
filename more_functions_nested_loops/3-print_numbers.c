0aa6531032ae2fb26ad6440dd06d3cc403e42d28
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 10:37:14 +0100

    print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_numbers - Entry point.
+* @a: number 1.
+* @b: number 2.
+* Return: Returns 1 if c is digit.
+*		Returns 0 otherwise.
+*/
+
+void print_numbers(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar (i);
+	}
+
+		_putchar('\n');
+}
+
+
