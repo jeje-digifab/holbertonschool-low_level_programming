b1d3fd5ea65cfc87affa7590a0f0c137e716e84c
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 10:10:15 +0100

    isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _isupper - Entry point
+* Return: Returns 1 if c is uppercase
+* 		Returns 0 otherwise
+*/
+
+int _isupper(int c)
+
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return(0);
+	}
+
+}
+
+
