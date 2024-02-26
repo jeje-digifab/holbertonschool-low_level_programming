f254a6df0d85c4f8821fb3c80c4f0ed6a5dc1319
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 11:02:49 +0100

    print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_most_numbers - Entry point.
+* Return: Returns number.
+*/
+
+void print_most_numbers(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+
+		_putchar(i);
+	}
+
+		_putchar('\n');
+}
+
+
