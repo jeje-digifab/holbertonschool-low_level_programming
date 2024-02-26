5b29d22dca11b425a3771d423c637785ec5a2a9b
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-26 23:15:47 +0100

    print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_line - Entry point.
+* @n: is the number of times the character _ should be printed
+* Return: Returns number.
+*/
+
+void print_diagonal(int n)
+
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for( i = 0; i < n; i++)
+		{
+			for( j = 0; j < n; j++)
+			{
+				if(j == i)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
