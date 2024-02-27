287fe902fffe5a2559ef3ec60771802d35838762
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 19:07:37 +0100

    puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point.
+ * @str: variable.
+ * Return: function that prints a string, followed by a new line, to stdout.
+ */
+
+void _puts(char *str)
+
+
+{
+	int i = 0;
+
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
+
