8b59831272de412742c264f1d5d3bb7ce090667c
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 13:18:46 +0100

    swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* swap_int - Entry point.
+* @a: variable.
+* @b: variable.
+* Return: function that takes a pointer to an int as parameter and updates
+ *			the value it points to to 98.
+*/
+
+void swap_int(int *a, int *b)
+
+{
+
+	*a = *a + *b;
+
+	*b = *a - *b;
+
+	*a = *a - *b;
+
+
+}
