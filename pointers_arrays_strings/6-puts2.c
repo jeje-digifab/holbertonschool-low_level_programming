3bb86e571c0818ace9976ece992d9a6d382baefd
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 22:15:25 +0100

    puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - Entry point.
+ * @str: variable.
+ * Return: function that returns the length of a string.
+ */
+
+void puts2(char *str)
+
+{
+	int i;
+
+	i = 0;
+
+
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+		i++;
+	}
+
+
+	_putchar('\n');
+}
