feaa67c9a9475c86f172b984fc1b1ff05fc90df5
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 09:56:16 +0100

    fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -24,18 +24,21 @@ int main(void)
 
 				if (i % 15 == 0)
 				{
-					printf("FizzBuzz ");
+					printf("FizzBuzz");
+					putchar(' ');
 				}
 
 				else if (i % 5 == 0)
 				{
 
-					printf("Buzz ");
+					printf("Buzz");
+					putchar(' ');
 				}
 
 				else if (i % 3 == 0)
 				{
-					printf("Fizz ");
+					printf("Fizz");
+					putchar(' ');
 				}
 			}
 
