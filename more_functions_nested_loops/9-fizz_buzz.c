6dcf59b37e213353f33dcafef4eac77b8f105dd2
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 09:45:41 +0100

    fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,42 +18,52 @@ int main(void)
 
 		for ( i = 1; i <= 100; i++)
 		{
-			if (i <= 9)
-			{
-				putchar(i + '0');
-				putchar(' ');
-			}
 
-			else if (i > 9 && i < 100)
+			if (i % 15 == 0 || i % 5 == 0 || i % 3 == 0)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
+
+				if (i % 15 == 0)
+				{
+					printf("FizzBuzz ");
+				}
+
+				else if (i % 5 == 0)
+				{
+
+					printf("Buzz ");
+				}
+
+				else if (i % 3 == 0)
+				{
+					printf("Fizz ");
+				}
 			}
 
 			else
 			{
-				putchar((i / 100) + '0');
-				putchar((i % 10) + '0');
-				putchar((i % 10) + '0');
+
+
+				if (i <= 9)
+				{
+					putchar(i + '0');
+					putchar(' ');
+				}
+
+				else if (i > 9 && i < 100)
+				{
+					putchar((i / 10) + '0');
+					putchar((i % 10) + '0');
+					putchar(' ');
+				}
+
+				else
+				{
+					putchar((i / 100) + '0');
+					putchar((i % 10) + '0');
+					putchar((i % 10) + '0');
+				}
+
 			}
-
-			if (i % 15 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-
-			else if (i % 5 == 0)
-			{
-
-				printf("Buzz ");
-			}
-
-			else if (i % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-
 		}
 		putchar('\n');
 		return (0);
