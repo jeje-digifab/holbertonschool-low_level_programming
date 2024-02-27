c06d9ac86109705e353586976871351b2d7f9c9e
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 13:52:16 +0100

    strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point.
+* @s: variable.
+* Return: function that takes a pointer to an int as parameter and updates
+ *			the value it points to to 98.
+*/
+
+int _strlen(char *s)
+
+{
+	return (*s);
+}
