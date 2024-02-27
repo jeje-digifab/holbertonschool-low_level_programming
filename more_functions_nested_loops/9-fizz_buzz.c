8db7cc011340272fbfe20628ec47ea2b5aa7414b
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 10:11:46 +0100

    Betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,62 +13,46 @@ int main(void)
 
 {
 	int i;
-
-
-
-		for ( i = 1; i <= 100; i++)
+		for (i = 1; i <= 100; i++)
 		{
-
 			if (i % 15 == 0 || i % 5 == 0 || i % 3 == 0)
 			{
-
 				if (i <= 99)
 				{
-
 					if (i % 15 == 0)
 					{
 						printf("FizzBuzz");
 						putchar(' ');
 					}
-
 					else if (i % 5 == 0)
 					{
-
 						printf("Buzz");
 						putchar(' ');
 					}
-
 					else if (i % 3 == 0)
 					{
 						printf("Fizz");
 						putchar(' ');
 					}
 				}
-
 				else
 				{
 					if (i % 15 == 0)
 					{
 						printf("FizzBuzz");
 					}
-
 					else if (i % 5 == 0)
 					{
-
 						printf("Buzz");
 					}
-
 					else if (i % 3 == 0)
 					{
 						printf("Fizz");
 					}
 				}
 			}
-
 			else
 			{
-
-
 				if (i <= 9)
 				{
 					putchar(i + '0');
