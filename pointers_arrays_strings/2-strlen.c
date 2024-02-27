0b0fc7b2123b959c8fc2f4cfafa13e0cdbb7d672
jeje-digifab <j.romand.digifab@gmail.com>
2024-02-27 18:36:32 +0100

    Description

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,7 @@
 /**
  * _strlen - Entry point.
  * @s: variable.
- * Return: function that takes a pointer to an int as parameter and updates
- *			the value it points to to 98.
+ * Return: function that returns the length of a string.
  */
 
 int _strlen(char *s)
