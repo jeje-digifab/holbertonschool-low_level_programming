24d8efdbd44e054257b461320a2570a846cda79a
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-01 13:05:49 +0100

    puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point.
+ * @str: variable.
+ * Return: unction that prints half of a string.
+ */
+
+void puts_half(char *str)
+
+{
+	int length, st, i;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+		{
+		st = length / 2;
+		}
+
+	else
+	{
+		st = (length + 1) / 2;
+	}
+
+	for (i = st; i <= length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
