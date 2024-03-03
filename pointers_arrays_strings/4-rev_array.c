17ec5b088032f9f6458ed2ea0e6082352c0746cf
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-03 21:38:02 +0100

    Betty

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,28 +2,27 @@
 
 
 /**
- * reverse_array - concatenates two strings with a
- *				specified number of bytes from src.
+ * reverse_array - function that reverses the content of an array
  * @a: pointer to the destination string
  * @n: maximum number of bytes from src to be used in the concatenation.
  *
- * Return: a pointer to the resulting string dest.
+ * Return: function that reverses the content of an array of integers.
  */
 
 void reverse_array(int *a, int n)
 
 
 {
-    int i = 0;
-    int j = n - 1;
-    int temp;
+	int i = 0;
+	int j = n - 1;
+	int temp;
 
-    while (i < j)
-    {
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-        i++;
-        j--;
-    }
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
 }
