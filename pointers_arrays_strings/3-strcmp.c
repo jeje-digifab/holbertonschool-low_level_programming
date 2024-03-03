8720cebfc450b9808505ea5029a0f180a6b6d6a7
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-03 18:30:24 +0100

    Correction de code

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strcmp - function that compares two strings s1 and s2.
  * @s1: first strings.
@@ -28,20 +28,14 @@ int _strcmp(char *s1, char *s2)
 		lengths2++;
 	}
 
-
-	if (*(s1) > *(s2))
-	{
-		return (15);
-	}
-
-	else if (*(s1) == *(s2))
+	if (s1 == s2)
 	{
 		return (0);
 	}
 
 	else
 	{
-		return (-15);
+		return (*s1 - *s2);
 	}
-
 }
+
