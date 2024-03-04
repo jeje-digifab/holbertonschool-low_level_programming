cd9540532e07d0d41492b8dda4c6a3efdf39a23e
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-04 14:55:55 +0100

    memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,14 +13,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	if (*dest <= *src)
+	if (n > 0)
 	{
 			for (i = 0; i < n ; i++)
 			{
-				*dest = *src;
-				dest++;
+				dest[i] = src[i];
 			}
 	}
-
 	return (dest);
 }
