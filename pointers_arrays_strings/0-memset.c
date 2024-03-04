db437a139f03ac1e6b25d7ea7d8335ba7006ee4b
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-04 13:05:07 +0100

    memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point.
+ * @s: pointeur vers la mémoire à remplir.
+ * @b: valeur constante à remplir.
+ * @n: longueur de la zone mémoire à remplir.
+ * Return: function that prints a sing, followed by a new line, to stdout.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+		for (i = 0; i < n ; i++)
+		{
+			*s = b;
+		}
+
+	return (s);
+}
