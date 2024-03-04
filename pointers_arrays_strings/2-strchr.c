6b269510fda3140ad6e12bb832b5db656d9b888a
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-04 16:29:39 +0100

    strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Entry point.
+ * @s: string.
+ * @c: character.
+ * Return: Returns a pointer to the first occurrence of the character c in the
+ *			string s, or NULL if the character is not found.
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] == c)
+		{
+			return (&s[len]);
+		}
+		len++;
+	}
+	return (NULL);
+}
