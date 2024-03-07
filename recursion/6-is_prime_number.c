a42fa742828d8c21a2dcbba720de5bf00ab9853a
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-07 22:51:28 +0100

    Add is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Entry point.
+ * @n: number.
+ * Return: function that returns the natural square root of a number.
+ */
+
+int is_prime_number(int n)
+
+{
+
+
+}
+
