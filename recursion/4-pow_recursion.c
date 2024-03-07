b8e845d34ea18de61376dae75d50f57c1eeb81b4
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-07 14:32:10 +0100

    pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -15,12 +15,12 @@ int _pow_recursion(int x, int y)
 
 	if (y < 0)
 	{
-		return (_pow_recursion(x, y +1 / x));
+		return (_pow_recursion(x, y + 1 / x));
 	}
 
-	else
+	else if (y == 0)
 	{
-		return (-1);
+		return (1);
 	}
 
 	return ( _pow_recursion (x, y - 1) * x);
