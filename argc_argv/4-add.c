0bac6d45301a26dda05d13c947a06561330419fd
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-08 22:56:59 +0100

    add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point.
+ * @argc: pointeur la phrase à copier.
+ * @argv: pointeur la phrase à copier.
+ * Return: fprogram that prints its name.
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i, sum;
+
+
+	if (argc > 1)
+	{
+		for (i = 1; i <= argc; i++)
+		{
+			sum = 0;
+			sum += atoi(argv[i]);
+			printf("%d\n", sum);
+			return (0);
+		}
+	}
+
+	else if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+/*
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+*/
+	return (0);
+}
