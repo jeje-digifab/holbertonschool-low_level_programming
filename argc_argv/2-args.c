55f239b06f04194d4f703171670ad752ad355c77
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-08 15:11:42 +0100

    args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: pointeur la phrase à copier.
+ * @argv: pointeur la phrase à copier.
+ * Return: fprogram that prints its name.
+ */
+
+int main(int argc, char *argv[])
+
+
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+
+	printf("\n");
+}
+
