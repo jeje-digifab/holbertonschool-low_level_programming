f7067cbbc5b2134cc27bd354764cbc39565524bb
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-12 13:04:51 +0100

    strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point.
+ * @str: pointeur la phrase à copier.
+ * Return: program that prints its name.
+ */
+
+char *_strdup(char *str)
+
+{
+
+	unsigned int i, len;
+	char *buffer;
+
+	len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] !='\0')
+	{
+		len++;
+	}
+
+
+	buffer = malloc((len + 1));
+	if (!buffer)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		buffer = str;
+	}
+	putchar('\0');
+
+
+	return (buffer);
+
+}
