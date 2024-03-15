5b1851141aebcf3dc705ab5d1209e8ecf81cb3f2
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-15 14:32:32 +0100

    array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,20 +16,18 @@ int *array_range(int min, int max)
 	int *array;
 	int i;
 
-
-
 	if (min > max)
 	{
 		return (NULL);
 	}
-	array = malloc((max - min + 1) * sizeof(int*));
+	array = malloc((max - min + 1) * sizeof(int *));
 
 	if (!array)
 	{
 		return (NULL);
 	}
 
-	for (i = min ; i <= max ; i++)
+	for (i = 0 ; i <= max - min ; i++)
 	{
 		array[i] = min + i;
 	}
