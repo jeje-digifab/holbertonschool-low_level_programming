0c9f0f2e48f9b57789dbf7d45925a3f02efbfefe
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-15 16:27:21 +0100

    Betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,22 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * dog - structure.
+ * @name: un pointeur vers une chaîne de caractères
+ *			représentant le nom du chien.
+ * @age: un nombre à virgule flottante
+ *			représentant l'âge du chien.
+ * @owner: un pointeur vers une chaîne de caractères
+ *			représentant le nom du propriétaire du chien.
+ */
+
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif /* !DOG_H_ */
