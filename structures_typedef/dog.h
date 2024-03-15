62c603f77b45490d9f400ceedae93fad0367755c
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-16 00:29:11 +0100

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,7 +11,6 @@
  *			représentant le nom du propriétaire du chien.
  */
 
-
 typedef struct dog
 {
 	char *name;
@@ -21,5 +20,7 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* !DOG_H_ */
