f1013505e8f5d1e31d1724761ced0740aa9ce7c9
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-16 16:52:09 +0100

    new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - creates a new dog structure with the given name, age, and owner
+ * @name: the name of the dog (must be duplicated)
+ * @age: the age of the dog
+ * @owner: the owner of the dog (must be duplicated)
+ *
+ * Return: a pointer to the new dog structure, or NULL if the function fails
+ *
+ * This function creates a new dog structure and initializes its name, age,
+ * and owner fields with the given values. The name and owner strings are
+ * duplicated using malloc and strcpy, so the caller does not need to keep
+ * the original strings around. If any memory allocation fails, the function
+ * frees any previously allocated memory and returns NULL.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j;
+	dog_t *new_dog;
+
+	i = 0;
+	j = 0;
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (!new_dog)
+	{
+		return (NULL);
+	}
+
+	while (name[i] != '\0')
+	{
+		i++;
+	}
+
+	while (owner[j] != '\0')
+	{
+		j++;
+	}
+
+	new_dog->name = malloc(i * sizeof(char) + 1);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(j * sizeof(char) + 1);
+	if (!new_dog->owner)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
+	return (new_dog);
+}
