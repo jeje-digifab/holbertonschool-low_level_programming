9027c6e9d6f36af0aacc6e636f1a2d1840435e04
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-18 13:09:57 +0100

    Update function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,16 +3,6 @@
 
 #include <stddef.h>
 
-/**
- * struct dog - structure.
- * @name: un pointeur vers une chaîne de caractères
- *			représentant le nom du chien.
- * @age: un nombre à virgule flottante
- *			représentant l'âge du chien.
- * @owner: un pointeur vers une chaîne de caractères
- *			représentant le nom du propriétaire du chien.
- */
-
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
