be0e8bbc1cf2c26e2f0b1e2d221721a64aca3464
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-19 13:57:53 +0100

    op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,82 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int a, b, result;
+
+
+/**
+ * op_add - Returns the sum of a and b.
+ *
+ * @a: Number 1.
+ * @b: Number 2.
+ *
+ * Return: Returns the sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * op_sub - Returns the difference of a and b.
+ *
+ * @a: Number 1.
+ * @b: Number 2.
+ *
+ * Return: Returns the difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return a - b;
+}
+
+/**
+ * op_mul - Returns the product of a and b.
+ *
+ * @a: Number 1.
+ * @b: Number 2.
+ *
+ * Return: Returns the product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * op_div - Returns the result of the division of a by b.
+ *
+ * @a: Number 1.
+ * @b: Number 2.
+ *
+ * Return: Returns the remainder of the division of a by b.
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Cannot divid by 0");
+		return (NULL);
+	}
+	return a / b;
+}
+
+/**
+ * op_mod - Returns the remainder of the division of a by b.
+ *
+ * @a: Number 1.
+ * @b: Number 2.
+ *
+ * Return: Returns the remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	return a % b;
+}
