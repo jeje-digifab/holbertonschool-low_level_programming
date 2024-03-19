b038105e8135f696d87359a6bd6cea1d7b744880
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-19 13:55:05 +0100

    Add calc.h

--- function_pointers/calc.h
+++ function_pointers/calc.h
@@ -7,6 +7,8 @@
  * @op: The operator
  * @f: The function associated
  */
+
+
 typedef struct op
 {
 	char *op;
@@ -14,5 +16,10 @@ typedef struct op
 } op_t;
 
 int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 #endif /* !CALC_H_ */
