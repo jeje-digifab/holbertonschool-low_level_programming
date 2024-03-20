cae7b69febec8b442d9e38002ee93ccd08aac852
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-20 19:03:30 +0100

    main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,31 +1,53 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 
 /**
- * main - check the code
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
  *
- * Return: Always 0.
+ * Description: This function checks the number of arguments, retrieves the
+ *              operator function using the get_op_func function, validates
+ *              the operator, performs the operation with the given numbers,
+ *              and prints the result. It handles errors such as invalid
+ *              number of arguments, invalid operator, and division by zero.
+ *
+ * Return: 0 on success, 98 for invalid number of arguments, 99 for invalid
+ *         operator, and 100 for division by zero.
  */
 
 int main(int argc, char *argv[])
-
 {
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
 	if (argc != 4)
 	{
-		printf("Entrez un nombre d'arguments valide: %s \n", argv[0]);
-		return (1);
+		printf("Error: Invalid number of arguments\n");
+		exit(98);
 	}
-	int num1 = atoi(argv[1]);
-	char operation = argv[2][0];
-	int num2 = atoi(argv[3]);
 
-	for (int i = 0; ops[i].op == operation)
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
 	{
-		int result = ops[i].func(num1, num2);
-		printf("results: %d \n", result);
-		return (0);
+		printf("Error\n");
+		exit(99);
 	}
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	result = op_func(num1, num2);
+
+	if (result == -1)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", result);
+	return (0);
 }
